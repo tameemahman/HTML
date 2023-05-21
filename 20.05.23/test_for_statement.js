24bd77a8b9d79d1d739d8dbba1c27e6cc3fc3359
Tameem <tameem.handb@gmail.com>
2023-05-21 12:57:05 +0600

    practice if & for

---
+++ 20.05.23/test_for_statement.js
@@ -0,0 +1,34 @@
+// for (let i=1; i<=100;i++){
+
+//     if(i%2==0){
+      
+//     }
+
+
+// else {
+//     console.log(i);
+// }
+
+
+// }
+
+
+// for(let i=1; i<=50; i++){
+
+//     if(i%3==0 || i%5==0){ 
+// console.log(i);
+//     }
+  
+    
+// else{}
+
+// }
+
+let input=29;
+let result=1;
+
+for(let i=input; i>0; i--){
+
+    result=result*i;
+}
+console.log(result);
